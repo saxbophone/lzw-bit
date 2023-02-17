b65d6083cbe9ba64920b01f889e2a72205af3b73
Joshua Saxby <joshua.a.saxby@gmail.com>
2023-02-17 20:27:55 +0000

    some more data analysis stuff for more efficient encoding formats

--- mixed_base_packing.py
+++ mixed_base_packing.py
@@ -43,7 +43,20 @@ from math import prod
 def bitlength(iterable):
     return sum(prod(it).bit_length() for it in iterable)
 
-from itertools import zip_longest
+# from itertools import zip_longest
 
-for m, d in zip_longest(Max.determine_packing(range(3, 101)), Dense.determine_packing(range(33, 101)), fillvalue=()):
-    print(m, d)
+# for m, d in zip_longest(Max.determine_packing(range(3, 101)), Dense.determine_packing(range(33, 101)), fillvalue=()):
+#     print(m, d)
+
+
+def saxby(bases, m, n):
+    return sum(prod(bases[y] for y in range(0, x)) for x in range(m, n + 1))
+
+
+def compare(start, end):
+    # comparing raw encoded length vs with Dense packing, and then Dense with length-detection using Saxby sequences
+    return (
+        bitlength(((i,) for i in range(start, end + 1))),
+        bitlength(Dense.determine_packing(range(start, end + 1))),
+        bitlength((saxby(bases, 0, len(bases)),) for bases in Dense.determine_packing(range(start, end + 1)))
+    )
